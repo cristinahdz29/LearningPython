344280e3302ae8ec2683bdadcd3e66e2db048532
Cristina Hernandez <64177857+cristinahdz29@users.noreply.github.com>
2020-09-09 12:29:17 -0400

    added calculate tip function

---
+++ hello.py
@@ -0,0 +1,14 @@
+
+# getting input from user in this case integers and adding together. this example contains integers and floats
+# always have to put int function in front of variables when concatenating because input function always returns strings, which will not add but concatenate
+
+
+# caclculate tip function
+
+def calculate_tip(total_cost, percentage):
+    tip = total_cost * (percentage/100)
+    return tip
+
+
+print(calculate_tip(100, 10)
+print(tip)
