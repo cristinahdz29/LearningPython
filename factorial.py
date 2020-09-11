8df65ce98d538f53e12270c72657444cec009f32
Cristina Hernandez <64177857+cristinahdz29@users.noreply.github.com>
2020-09-10 22:18:21 -0400

    updated code

--- factorial.py
+++ factorial.py
@@ -4,7 +4,9 @@ def factorial_problem():
     number = int(input("Tell me a number:"))
     for i in range(1, number):
         number = number * i
+        print(i)
     print(number)
+
 
 
 factorial_problem()
--- prime.py
+++ prime.py
@@ -3,18 +3,23 @@
 # prime number is always greater than 1
 
 def prime_function():
+    # set a variable equal to true, so it assumes that it is prime
     is_prime = True
+    # Obtaining input from the user, and converting it to an integer so we can use it in the function
     number = int(input("Enter any number: "))
 
-    if number > 1:
+    if number < 1:
+        return("This number is neither composite nor prime")
+    else:
         for i in range(2, number):
             if (number % i) == 0:
                 is_prime = False
                 break
+
     if is_prime == True:
-        print("The number is prime")
+        return("The number is prime")
     else:
-        print("Not a prime number")
+        return("Not a prime number")
         
 
-prime_function()
+print(prime_function())
