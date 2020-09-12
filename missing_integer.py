7f2cbc812b39740f074603af22c3552b6f4e20bd
Cristina Hernandez <64177857+cristinahdz29@users.noreply.github.com>
2020-09-11 20:13:34 -0400

    converted into a function

--- missing_integer.py
+++ missing_integer.py
@@ -5,9 +5,9 @@ numbers = [0, 1, 2, 3, 5, 6, 7, 8, 9]
 #     if i + 1 == numbers:
 #         print(missing)
 
-
-for x in range(0, len(numbers) - 1):
-    numb1 = numbers[x+1]
-    numb2 = numbers[x]
-    if numb1 - numb2 != 1:
-        print(numbers[x] + 1)
+def missing_integer(arr):
+    for x in range(0, len(arr) - 1):
+        numb1 = arr[x+1]
+        numb2 = arr[x]
+        if numb1 - numb2 != 1:
+            print(arr[x] + 1)
